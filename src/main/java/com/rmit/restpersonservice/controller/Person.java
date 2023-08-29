4cfe5aaac6f8533099a74761324a5466fc2a721e
Alessio Bonti <alexbonti83@hotmail.com>
2023-08-30 08:41:36 +1000

    added workflow

--- src/main/java/com/rmit/restpersonservice/controller/Person.java
+++ src/main/java/com/rmit/restpersonservice/controller/Person.java
@@ -5,7 +5,6 @@ import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
 
 
-
 @RestController
 public class Person {
 
@@ -18,6 +17,4 @@ public class Person {
     public String Greet(@RequestParam String name){
         return "Hello to you my dear visitor " +name;
     }
-
-
 }
